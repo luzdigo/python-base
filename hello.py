fcb915d99ea682e5ae134660135ba6e2073ad51e
luz_digo <rootaviooo@gmail.com>
2024-04-25 12:28:17 -0300

    Imprime Hello, World em multi línguas

--- hello.py
+++ hello.py
@@ -1,1 +1,29 @@
-print ('teste')
+#!/usr/bin/env python
+
+"""Imprime Hello, World em multi línguas
+
+Como usar:
+
+Tenha a linguagem devidamente configurada, ex:
+    
+    export LANG=pt_BR
+
+Execução:
+
+    Python Hello.py
+"""
+
+__author__ = "Rodrigo"
+__version__ = "0.0.1"
+
+
+
+import os
+
+langs = os.getenv ("LANG", "en_US") [:5]
+msg = "Hello, World"
+
+if langs == "en_US":
+    print ("Hello, World")
+elif langs == "pt_BR":
+    print ("Olá, mundo")
